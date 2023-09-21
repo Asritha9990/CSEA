5e74bcfc0acdd32bbcf60279f12f7e7957a9b7ab
Admin <Admin@DESKTOP-2S7HFKC>
2023-09-21 14:02:43 +0530

    team lead commit

---
+++ src/com/lbrce/csea/Sample.java
@@ -0,0 +1,14 @@
+package com.lbrce.csea;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      asritha();
+	}
+	public static void asritha( ) {
+		// TODO Auto-generated method stub
+		System.out.println("this is Team lead: Asritha");
+
+	}
+}
