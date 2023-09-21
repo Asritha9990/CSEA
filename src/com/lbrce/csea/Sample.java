a5f7cd9145b5a47a55592539cce939a19264db44
AdmiN <AdmiN@DESKTOP-M101FJ5>
2023-09-21 14:54:15 +0530

    hello lead

--- src/com/lbrce/csea/Sample.java
+++ src/com/lbrce/csea/Sample.java
@@ -5,10 +5,22 @@ public class Sample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       asritha();
+      Pravallika();
+      ashi();
 	}
 	public static void asritha( ) {
 		// TODO Auto-generated method stub
 		System.out.println("this is Team lead: Asritha");
+
+	}
+	public static void Pravallika( ) {
+		// TODO Auto-generated method stub
+		System.out.println("this is Team member: Pravallika");
+
+	}
+	public static void ashi( ) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
 
 	}
 }
